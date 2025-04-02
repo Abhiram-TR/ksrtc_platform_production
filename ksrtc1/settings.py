ac1e7fb7f526ba13aaf270bc7e965f95f00093ef
jeevjacobgeorge <jeevjacobgeoge@gmail.com>
2025-04-02 17:04:32 +0530

    https:

--- ksrtc1/settings.py
+++ ksrtc1/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "ksrtc1-294298529874.us-central1.run.app",  
     "127.0.0.1",  # Keep localhost for local testing
-    "https://*.run.app",
+    "*.run.app",
     "ksrtc1-945909242090.us-central1.run.app"
 ]
 
